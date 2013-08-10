782cad7285168c2e4943a2fe23d1ee8b529acef8
Murtaza Gulamali <mygulamali@gmail.com>
2013-08-10 07:28:07 +0100

    Add month names.

---
+++ lib/hijri_date/month_names.rb
@@ -0,0 +1,29 @@
+module HijriDate
+  MONTHNAMES = [nil,
+                'Moharram al-Haraam',
+                'Safar al-Muzaffar',
+                'Rabi al-Awwal',
+                'Rabi al-Aakhar',
+                'Jumada al-Ula',
+                'Jumada al-Ukhra',
+                'Rajab al-Asab',
+                'Shabaan al-Karim',
+                'Ramadaan al-Moazzam',
+                'Shawwal al-Mukarram',
+                'Zilqadah al-Haraam',
+                'Zilhaj al-Haraam']
+  
+  SHORTMONTHNAMES = [nil,
+                     'Moharram',
+                     'Safar',
+                     'Rabi I',
+                     'Rabi II',
+                     'Jumada I',
+                     'Jumada II',
+                     'Rajab',
+                     'Shabaan',
+                     'Ramadaan',
+                     'Shawwal',
+                     'Zilqadah',
+                     'Zilhaj']
+end
