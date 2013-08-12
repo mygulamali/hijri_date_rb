6f38ad32042d454a0be4d3fafd38101cae897207
Murtaza Gulamali <mygulamali@gmail.com>
2013-08-12 23:36:56 +0100

    Add tests.

--- test/hijri_date_test.rb
+++ test/hijri_date_test.rb
@@ -1,9 +1,33 @@
+require 'date'
+require 'hijri_date'
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'hijri_date'
 
 class HijriDateTest < MiniTest::Unit::TestCase
-  def test_truth
-    assert true
+  def setup
+    @date = HijriDate::Date.new  # 20/04/1432H = 25/03/2011AD
+    @ajd = Date.new(2011, 3, 25).jd - 0.5
+  end
+
+  def test_kabisa_year
+    refute @date.is_kabisa?
+    assert @date.is_kabisa? 1431
+  end
+
+  def test_days_in_month
+    assert_equal 29, @date.days_in_month
+    assert_equal 29, @date.days_in_month(12)
+    assert_equal 30, @date.days_in_month(12, 1431)
+  end
+
+  def test_day_of_year
+    assert_equal 109, @date.day_of_year
+    assert_equal  10, @date.day_of_year(HijriDate::Date.new(1432, 1, 10))
+    assert_equal 355, @date.day_of_year(HijriDate::Date.new(1431, 12, 30))
+    assert_equal 354, @date.day_of_year(HijriDate::Date.new(1432, 12, 29))
+  end
+
+  def test_ajd
+    assert_equal @ajd, @date.ajd
   end
 end
