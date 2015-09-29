d72083aab3304f2624ea4127b2fff02fe2277f6b
Murtaza Gulamali <mygulamali@gmail.com>
2015-09-29 09:22:04 +0100

    Make constant names more readable

--- lib/hijri_date.rb
+++ lib/hijri_date.rb
@@ -36,7 +36,7 @@ module HijriDate
     # day of the year corresponding to this (or specified) Hijri date
     def day_of_year(date = self)
       return date.day if date.month == 1
-      HijriDate::DAYSINYEAR[date.month - 2] + date.day
+      HijriDate::DAYS_IN_YEAR[date.month - 2] + date.day
     end
 
     # return Julian Day number associated with this (or specified) Hijri date
@@ -45,7 +45,7 @@ module HijriDate
       if (date.year % 30 == 0)
         return 1948084 + y30 * 10631 + day_of_year(date)
       else
-        return 1948084 + y30 * 10631 + HijriDate::DAYSIN30YEARS[date.year - y30 * 30 - 1] + day_of_year(date)
+        return 1948084 + y30 * 10631 + HijriDate::DAYS_IN_30_YEARS[date.year - y30 * 30 - 1] + day_of_year(date)
       end
     end
 
@@ -91,17 +91,17 @@ module HijriDate
     left -= y30 * 10631
     i = 0
 
-    i += 1 while left > HijriDate::DAYSIN30YEARS[i]
+    i += 1 while left > HijriDate::DAYS_IN_30_YEARS[i]
     year = (y30 * 30.0 + i).to_i
 
-    left -= HijriDate::DAYSIN30YEARS[i - 1] if i > 0
+    left -= HijriDate::DAYS_IN_30_YEARS[i - 1] if i > 0
     i = 0
 
-    i += 1 while left > HijriDate::DAYSINYEAR[i]
+    i += 1 while left > HijriDate::DAYS_IN_YEAR[i]
     month = (i + 1).to_i
 
     if i > 0
-      day = (left - HijriDate::DAYSINYEAR[i - 1]).to_i
+      day = (left - HijriDate::DAYS_IN_YEAR[i - 1]).to_i
     else
       day = left.to_i
     end
